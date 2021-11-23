37ffcdc5874fa21b67c3a02c115d6ab9123e263c
jcampb98 <Caboose768@gmail.com>
2021-11-23 11:16:39 +0000

    added return to about js

--- src/components/about.js
+++ src/components/about.js
@@ -1,35 +1,37 @@
 import React from "react";
 
 export default function About(){
-    <section id="about">
-        <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
-        <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-            Hi, I'm Joshua.
-            <br className="hidden lg:inline-block" />I love to code/program amazing
-            apps.
-          </h1>
-          <p className="mb-8 leading-relaxed">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
-            laborum quasi, incidunt dolore iste nostrum cupiditate voluptas?
-            Laborum, voluptas natus?
-          </p>
-          <div className="flex justify center">
-              <a href="#contact" 
-              className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
-                  Contact Me
-              </a>
-              <a href="#projects"
-              className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
-                  See my Past Work
-              </a>
-          </div>
-        </div>
-        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-            <img
-                className="object-cover object-center rounded"
-                alt="hero image"
-                src="./coding.svg"
-            />
-        </div>
-    </section>
+    return(
+        <section id="about">
+            <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
+            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+                Hi, I'm Joshua.
+                <br className="hidden lg:inline-block" />I love to code/program amazing
+                apps.
+            </h1>
+            <p className="mb-8 leading-relaxed">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
+                laborum quasi, incidunt dolore iste nostrum cupiditate voluptas?
+                Laborum, voluptas natus?
+            </p>
+            <div className="flex justify center">
+                <a href="#contact" 
+                className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
+                    Contact Me
+                </a>
+                <a href="#projects"
+                className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
+                    See my Past Work
+                </a>
+            </div>
+            </div>
+            <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+                <img
+                    className="object-cover object-center rounded"
+                    alt="hero image"
+                    src="./coding.svg"
+                />
+            </div>
+        </section>
+    );
 }
