38f143bd9f2dc96429eb9ab6b23db4f101d33723
jcampb98 <Caboose768@gmail.com>
2021-11-23 11:05:05 +0000

    added in components to app js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import React from "react";
+import About from "./components/about.js";
+import Contact from "./components/contact.js";
+import Navbar from "./components/navbar.js";
+import Projects from "./components/projects.js";
+import Skills from "./components/skills.js";
 
 export default function App() {
-  return "App";
+  return (
+    <main>
+      <Navbar/>
+      <About/>
+      <Projects/>
+      <Skills/>
+      <Contact/>
+    </main>
+  )
 }
