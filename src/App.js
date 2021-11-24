54b886803c8a68c8b36eba00e45f58de9c74a0ad
jcampb98 <Caboose768@gmail.com>
2021-11-24 16:16:58 +0000

    fixed some errors and updated project.js included a test page just to test feature

--- src/App.js
+++ src/App.js
@@ -14,5 +14,5 @@ export default function App() {
       <Skills/>
       <Contact/>
     </main>
-  )
+  );
 }
--- src/data.js
+++ src/data.js
@@ -4,23 +4,7 @@ export const projects = [
     subtitle: "MERN Stack",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-1.gif",
-    link: "https://reactbootcamp.com",
-  },
-  {
-    title: "React Tracks",
-    subtitle: "React and Python",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-2.gif",
-    link: "https://reactbootcamp.com",
-  },
-  {
-    title: "Epic Todo App",
-    subtitle: "React Hooks",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-4.gif",
+    image: "./public/project.gif",
     link: "https://reactbootcamp.com",
   },
 ];
