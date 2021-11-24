ff5c6f566b3af29df40812a676ce6b0b67567516
jcampb98 <Caboose768@gmail.com>
2021-11-24 15:35:11 +0000

    added the form for the contact page

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,1 +1,111 @@
-export default function Contact(){}
+import React from "react";
+
+export default function Contact(){
+    const [name, setName] = React.useState("");
+    const [email, setEmail] = React.useState("");
+    const [message, setMessage] = React.useState("");
+
+    function encode(data){
+        return Object.keys.apply(data)
+        .map(
+            (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
+        )
+        .join("&");
+    }
+
+    function handleSubmit(e){
+        e.preventDefault();
+        fetch("/", {
+            method: "POST",
+            headers: {"Content-Type": "application/x-www-form-urlencoded"},
+            body: encode({"form-name": "contact", name, email, message}),
+        })
+        .then(() => alert("Message sent!"))
+        .catch((error) => alert(error));
+    }
+    
+    return(
+        <section id="contact" className="relative">
+            <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
+                <iframe 
+                    width="100%"
+                    height="100%"
+                    title="map"
+                    className="absolute inset-0"
+                    frameborder={0}
+                    marginHeight={0}
+                    marginWidth={0}
+                    style={{filter: "opacity(0.7)"}}
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d143322.87425748174!2d-4.372540321355689!3d55.85538065264858!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x488815562056ceeb%3A0x71e683b805ef511e!2sGlasgow!5e0!3m2!1sen!2suk!4v1637767163985!5m2!1sen!2suk"
+                />
+                <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
+                    <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
+                    <h2 className="title-font font-semibold text-white tracking-widest text-xs">
+                        EMAIL
+                    </h2>
+                    <a className="text-indigo-400 leading-relaxed">
+                        campbelljoshua36@email.com
+                    </a>
+                    <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
+                        PHONE
+                    </h2>
+                    <p className="leading-relaxed">+44 7367 283414</p>
+                    </div>
+            </div>
+
+            <form
+                netlify
+                name="contact"
+                onSubmit={handleSubmit}
+                className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+                <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
+                    Hire Me
+                </h2>
+                <p className="leading-relaxed mb-5">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum
+                    suscipit officia aspernatur veritatis. Asperiores, aliquid?
+                </p>
+                <div className="relative mb-4">
+                    <label htmlFor="name" className="leading-7 text-sm text-gray-400">
+                    Name
+                    </label>
+                    <input
+                    type="text"
+                    id="name"
+                    name="name"
+                    className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                    />
+                </div>
+                <div className="relative mb-4">
+                    <label htmlFor="email" className="leading-7 text-sm text-gray-400">
+                    Email
+                    </label>
+                    <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                    />
+                </div>
+                <div className="relative mb-4">
+                    <label
+                    htmlFor="message"
+                    className="leading-7 text-sm text-gray-400">
+                    Message
+                    </label>
+                    <textarea
+                    id="message"
+                    name="message"
+                    className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+                    />
+                </div>
+                <button
+                    type="submit"
+                    className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+                    Submit
+                </button>
+            </form>
+            </div>
+        </section>
+    );
+}
