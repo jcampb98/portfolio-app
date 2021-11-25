2d7341fb395ea151b8e42f975d4dc6feb7395d2a
jcampb98 <Caboose768@gmail.com>
2021-11-25 08:03:52 +0000

    updated the skills section code by fixing the icon size

--- src/data.js
+++ src/data.js
@@ -4,7 +4,15 @@ export const projects = [
     subtitle: "MERN Stack",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./public/project.gif",
+    image: "./project.jpeg",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "React Reserve",
+    subtitle: "MERN Stack",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project.jpeg",
     link: "https://reactbootcamp.com",
   },
 ];
