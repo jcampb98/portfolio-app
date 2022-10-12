b085de3e2bcc8b584e9a49e49d0dafa1887c81a8
jcampb98 <caboose768@gmail.com>
2022-10-12 13:29:52 +0100

    fixed the error that was breaking the form on the website by appplying a fix

--- src/components/contact.js
+++ src/components/contact.js
@@ -5,23 +5,23 @@ export default function Contact(){
     const [email, setEmail] = React.useState("");
     const [message, setMessage] = React.useState("");
 
-    function encode(data){
-        return Object.keys.apply(data)
-        .map(
+    function encode(data) {
+        return Object.keys(data)
+          .map(
             (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
-        )
-        .join("&");
-    }
-
-    function handleSubmit(e){
+          )
+          .join("&");
+      }
+    
+    function handleSubmit(e) {
         e.preventDefault();
         fetch("/", {
-            method: "POST",
-            headers: {"Content-Type": "application/x-www-form-urlencoded"},
-            body: encode({"form-name": "contact", name, email, message}),
+          method: "POST",
+          headers: { "Content-Type": "application/x-www-form-urlencoded" },
+          body: encode({ "form-name": "contact", name, email, message }),
         })
-        .then(() => alert("Message sent!"))
-        .catch((error) => alert(error));
+          .then(() => alert("Message sent!"))
+          .catch((error) => alert(error));
     }
     
     return(
