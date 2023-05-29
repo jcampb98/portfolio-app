b638aed69a4c4c35f8092c33fea5bb670b5a5c00
jcampb98 <caboose768@gmail.com>
2023-05-29 14:45:21 +0100

    deploying update

--- src/components/contact.js
+++ src/components/contact.js
@@ -56,7 +56,9 @@ export default function Contact(){
                 </div>
 
                 <form
-                    netlify="true"
+                    netlify
+                    data-netlify="true"
+                    data-netlify-honeypot="bot-field"
                     name="contact"
                     onSubmit={handleSubmit}
                     className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
